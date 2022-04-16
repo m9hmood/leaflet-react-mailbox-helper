368670e78e5c9a98a23e601d3ea0bb855c2c30a8
Mahmood <mahmood3xq@gmail.com>
2022-04-16 23:36:23 +0300

    init commit

--- README.md
+++ README.md
@@ -1,70 +1,29 @@
-# Getting Started with Create React App
+# Leaflet Maps with React
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This app try to help Mailman to collect all the mailboxes he can collect in his way.
 
-## Available Scripts
+## Check the App online
 
-In the project directory, you can run:
+[Click Here](https://mailman-helper.surge.sh/)    
 
-### `npm start`
+## Run App on local
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+run `npm install` to install all the dependencies then run `npm start` to start the app.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
 
-### `npm test`
+## How to use
+**Click** on the marker icon in the top-left corner to start drawing your path.
+after you draw your path, you will side a menu to calculate and change the buffer distance.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## App Features
+- Each mailbox is a marker on the map.
+- When you click on a marker, you will see the mailbox's details.
+- You can draw your own route on the map.
+- Each marker in your route is a stop point.
+- Mailboxes will be calculated based on your route and distance.
+- You can remove stop points from your route by clicking on it.
+- You can add stop points to your route without redrawing your path.
+- Start point and end point have a different icon than stop point in your route.
+- App will calculate the distance between stop point and mailbox in meters.
+- you can change the buffer distance to calculate the mailboxes near the stop point easily from the settings.
+- if in any case the stop points will have shared mailboxes, the app will show only one in the list.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,270 @@
-import logo from './logo.svg';
 import './App.css';
+import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import {useCallback, useEffect, useRef, useState} from "react";
+import L from "leaflet";
+import "leaflet-draw";
+import "leaflet-routing-machine";
+import {randomMailboxPoints} from "./randomMailboxPoints";
+
+/**
+ * @typedef {Object} MapRef
+ * @property {L.Map} current
+ */
+
+
+const mailboxesPoints = randomMailboxPoints;
+// custom marker Icons
+const startIcon = L.icon({
+    iconUrl: 'images/map/start-flag.png',
+    iconSize: [30, 30], // size of the icon
+    iconAnchor: [15, 30], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -30] // point from which the popup should open relative to the iconAnchor
+});
+const stopIcon = L.icon({
+    iconUrl: 'images/map/stop-point.png',
+    iconSize: [30, 30], // size of the icon
+    iconAnchor: [15, 30], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -30] // point from which the popup should open relative to the iconAnchor
+});
+const endIcon = L.icon({
+    iconUrl: 'images/map/end-flag.png',
+    iconSize: [30, 30], // size of the icon
+    iconAnchor: [15, 30], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -30] // point from which the popup should open relative to the iconAnchor
+});
+const mailboxIcon = L.icon({
+    iconUrl: 'images/map/mailbox.png',
+    iconSize: [30, 30], // size of the icon
+    iconAnchor: [15, 30], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -30] // point from which the popup should open relative to the iconAnchor
+});
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    /**
+     * Map reference
+     * @type {MapRef}
+     */
+    const map = useRef();
+
+    const [distance, setDistance] = useState(100);
+    const [nearestMailboxes, setNearestMailboxes] = useState([]);
+    const [markers, _setMarkers] = useState([]);
+    const routing = useRef(null);
+    const mailboxesMarkers = useRef([]);
+    const coverageMarkers = useRef([]);
+
+    /**
+     * making refs to markers to keep it updated in the event listeners
+     * and to avoid useEffect to be called on every marker's update
+     */
+    const markersRef = useRef(markers);
+    const setMarkers = (markers) => {
+        markersRef.current = markers;
+        _setMarkers(markers);
+    };
+
+    const initMap = () => {
+        map.current = L.map('map').setView([33.3152, 44.3661], 13);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiaXNtYWhtb29kIiwiYSI6ImNsMXZjcHV5ZjAyaXUza3BsdWZsaG5heXoifQ.JpCyc1hAFZ8DxvxLDcMU0w'
+        }).addTo(map.current);
+        if(mailboxesMarkers.current.length < 1) {
+            mailboxesPoints.forEach((point, idx) => {
+                const marker = L.marker(point, {icon: mailboxIcon}).addTo(map.current);
+                marker.bindPopup(`Mailbox ${idx + 1}`);
+                mailboxesMarkers.current.push(marker);
+            });
+        }
+    }
+
+    const initDrawer = () => {
+        const drawControl = new L.Control.Draw({
+            draw: {
+                polyline: false,
+                polygon: false,
+                circle: false,
+                marker: {
+                    icon: stopIcon,
+                    repeatMode: true
+                },
+                circlemarker: false,
+                rectangle: false,
+            },
+            marker: {
+                icon: stopIcon //Here assign your custom marker
+            },
+            edit: {
+                featureGroup: new L.FeatureGroup(),
+                edit: false,
+                remove: false
+            },
+        });
+        map.current.addControl(drawControl);
+    }
+
+    const updateIcons = () => {
+        if (markersRef.current.length < 2) return;
+        markersRef.current[0].setIcon(startIcon);
+        markersRef.current[markersRef.current.length - 1].setIcon(endIcon);
+        for (let i = 1; i < markersRef.current.length - 1; i++) {
+            markersRef.current[i].setIcon(stopIcon);
+        }
+    }
+
+
+    const initDrawerListeners = useCallback(() => {
+        map.current.on(L.Draw.Event.CREATED, function (e) {
+            if (e.layerType === 'marker') {
+                e.layer.options.icon = markersRef.current.length > 0 ? stopIcon : startIcon;
+                setMarkers([...markersRef.current, e.layer]);
+                updateIcons()
+                e.layer.on('click', function (e) {
+                    e.target.remove();
+                    const coverage = coverageMarkers.current.find(cvMarker => cvMarker._latlng === e.target._latlng);
+                    if(coverage) coverage.remove();
+                    setMarkers(markersRef.current.filter(m => m !== e.target));
+                    updateIcons()
+                })
+            }
+            map.current.addLayer(e.layer)
+        });
+    }, [])
+
+
+    const initRoutingPath = (nMarkers) => {
+        if (nMarkers.length < 1) return;
+        if (!routing.current) {
+            routing.current = L.Routing.control({
+                waypoints: nMarkers.map(m => m._latlng),
+                routeWhileDragging: false,
+                show: false,
+                autoRoute: true,
+                lineOptions: {
+                    color: 'blue'
+                },
+                createMarker: () => false,
+                router: L.Routing.mapbox('pk.eyJ1IjoiaXNtYWhtb29kIiwiYSI6ImNsMXZjcHV5ZjAyaXUza3BsdWZsaG5heXoifQ.JpCyc1hAFZ8DxvxLDcMU0w')
+            }).addTo(map.current);
+        } else {
+            routing.current.setWaypoints(nMarkers.map(m => m._latlng));
+        }
+
+    }
+
+    function getNearestPoints(point, points) {
+        const nearest = [];
+        for (let i = 0; i < points.length; i++) {
+            const p = points[i];
+            const d = L.latLng(p[0], p[1]).distanceTo(point);
+            if (d <= distance) {
+                nearest.push({
+                    name: `Mailbox ${i + 1}`,
+                    point: p,
+                    distance: d
+                })
+            }
+        }
+        return nearest;
+    }
+
+    const highlightMailbox = (mailbox) => {
+        const mailboxMarker = mailboxesMarkers.current.find((m) => m._latlng.lat === mailbox.point[0] && m._latlng.lng === mailbox.point[1]);
+        mailboxMarker.openPopup();
+        map.current.fitBounds(mailboxMarker.getLatLng().toBounds(100));
+
+    }
+
+
+    const calculateMailboxes = () => {
+        let _tempMailboxes = [];
+        coverageMarkers.current.forEach(cvMarker => cvMarker.remove());
+        markers.forEach(marker => {
+            const nearest = getNearestPoints(marker._latlng, mailboxesPoints);
+            const cvMarker = L.circle(marker._latlng, {radius: distance}).addTo(map.current);
+            coverageMarkers.current.push(cvMarker)
+            _tempMailboxes = [..._tempMailboxes, ...nearest];
+        });
+        // remove duplicates to avoid duplicates in the list
+        const uniqueMailboxes = _tempMailboxes.reduce((acc, curr) => {
+            if (!acc.find(m => m.point[0] === curr.point[0] && m.point[1] === curr.point[1])) {
+                acc.push(curr);
+            }
+            return acc;
+        }, []);
+        setNearestMailboxes(uniqueMailboxes);
+
+    };
+
+
+    const resetMap = useCallback(() => {
+        setMarkers([]);
+        setNearestMailboxes([]);
+        coverageMarkers.current = [];
+        routing.current.setWaypoints([]);
+        routing.current = null;
+        mailboxesMarkers.current = [];
+        map.current.off();
+        map.current.remove();
+        initMap();
+        initDrawer()
+        initDrawerListeners();
+    }, [initDrawerListeners])
+
+    // init map and drawer
+    useEffect(() => {
+        if(!map.current) {
+            initMap();
+            initDrawer();
+        }
+    }, [])
+    // init drawer listeners
+    useEffect(() => {
+        initDrawerListeners()
+    }, [initDrawerListeners])
+    // init routing on markers change
+    useEffect(() => {
+        if (markers.length > 1) {
+            initRoutingPath(markers);
+        }
+    }, [markers])
+
+
+    return (
+        <div className="App">
+            {markers.length > 1 && <div className={'overlay-sidebar'}>
+                <div className={'overlay-sidebar__body'}>
+                    <div className={'overlay-sidebar__input'}>
+                        <label>
+                            Buffer Distance in meters
+                            <input type="number" value={distance} onChange={(e) => setDistance(e.target.value)}/>
+                        </label>
+                    </div>
+                    <button className={'btn btn--block'} onClick={calculateMailboxes}>
+                        Calculate Mailboxes
+                    </button>
+                    <button className={'btn btn--danger btn--block'} onClick={resetMap}>
+                        Reset Map
+                    </button>
+                    <p>Mailboxes You can collect in your way: {nearestMailboxes.length}</p>
+
+                    <ul className={'overlay-sidebar__list'}>
+                        {nearestMailboxes.map(mailbox => {
+                            return <li key={mailbox.name} onClick={() => highlightMailbox(mailbox)}>{mailbox.name}</li>
+                        })}
+                    </ul>
+                </div>
+            </div>}
+            <div id="map"/>
+        </div>
+    );
 }
 
 export default App;
